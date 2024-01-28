6309d4ec939f9f08b1de242dff491c977a8a33ca
Aleksandra <alexandra.bakhcheva@gmail.com>
2024-01-28 22:45:41 +0300

    add localStorage

--- filters-list_react-redux/src/index.js
+++ filters-list_react-redux/src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./store/index";
+import { configureStore } from "./store/index";
 import "./index.css";
 import App from "./App";
+
+const store = configureStore();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
